e81b40d16f04b98aa86718980f78f0325fe45ec6
youri47 <youri93400@gmail.com>
2025-10-20 22:39:46 +0000

    adding chmod +x

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
