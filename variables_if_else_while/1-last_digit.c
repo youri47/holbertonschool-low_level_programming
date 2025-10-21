154dfcc867579ed814fdb18eaa19ec19e015e558
youri47 <youri93400@gmail.com>
2025-10-21 12:51:43 +0000

    adding chmod +x

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a random number and categorizes it.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+
+	printf("Last digit of %d is %d", n, lastDigit);
+
+	if (lastDigit > 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else if (lastDigit == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else /* lastDigit is less than 6 and not 0 */
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
