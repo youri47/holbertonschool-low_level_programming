3e38baa7d1596cb77b962870d44896b96b03edb4
youri47 <youri93400@gmail.com>
2025-10-22 21:23:05 +0000

    chmod +x

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; text[i] != '\0'; i++)
+		_putchar(text[i]);
+	_putchar('\n');
+
+	return (0);
+}
