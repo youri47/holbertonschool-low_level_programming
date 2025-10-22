4110a1721b7f054a1c23984be1370126f1bf1bac
youri47 <youri93400@gmail.com>
2025-10-22 11:06:12 +0000

    addind chmod +x

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int singleDigit = 0;
+
+    while (singleDigit < 10 )
+    {
+        putchar(singleDigit + '0'); 
+        singleDigit++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
