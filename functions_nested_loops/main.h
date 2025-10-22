577791ff424af60d46eb30c60fda3ca33e8c22a0
youri47 <youri93400@gmail.com>
2025-10-22 21:42:37 +0000

    chmod +x

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints "_putchar" followed by a new line
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char text[] = "_putchar";
-	int i;
-
-	for (i = 0; text[i] != '\0'; i++)
-		_putchar(text[i]);
-	_putchar('\n');
-
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
+
