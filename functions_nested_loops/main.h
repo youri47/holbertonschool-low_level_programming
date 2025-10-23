c0644bddc179d3c093d81ab87bb51d4afb75e965
youri47 <youri93400@gmail.com>
2025-10-23 15:36:49 +0000

    chmod +x

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,5 +9,10 @@ int _isalpha(int c);  /* <--- ici */
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
 #endif
