4e79ded61e730aa47727e903a74dbe842cf385f8
youri47 <youri93400@gmail.com>
2025-10-23 22:48:11 +0000

    chmod +x

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -36,42 +36,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-#include "main.h"
-
-/**
- * times_table - prints the 9 times table, starting with 0
- *
- * Return: void
- */
-void times_table(void)
-{
-	int a, b, result;
-
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			result = a * b;
-
-			if (b == 0)
-			{
-				_putchar(result + '0');
-			}
-			else if (result < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
 }
