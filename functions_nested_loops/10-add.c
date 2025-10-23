c5e6c6072117c2e316febfeb6261b3808d4aed51
youri47 <youri93400@gmail.com>
2025-10-23 22:50:19 +0000

    chmod +x

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Additionne deux entiers et renvoie le résultat
+ * @a: premier entier
+ * @b: second entier
+ *
+ * Return: la somme de a et b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
