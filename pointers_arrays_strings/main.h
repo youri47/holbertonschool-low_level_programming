6137f48fbaa81b4d64affd48890c0b7df418d1dc
youri47 <youri93400@gmail.com>
2025-11-06 23:09:14 +0000

    recursion

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,12 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
 
 #endif
---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: The base number
+ * @y: The exponent
+ *
+ * Return: x to the power of y, or -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
