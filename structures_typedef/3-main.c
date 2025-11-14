b539546176bfdeab7ca539c01fac7ab30f35a135
youri47 <youri93400@gmail.com>
2025-11-14 15:52:41 +0000

    structures_typedef

---
+++ structures_typedef/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+
+	return (0);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Structure représentant un chien
- * @name: Nom du chien (chaîne de caractères)
- * @age: Âge du chien (nombre à virgule flottante)
- * @owner: Propriétaire du chien (chaîne de caractères)
- *
- */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
