6791f347eb2931f600507b0d8abdab6c246b31a1
youri47 <youri93400@gmail.com>
2025-11-19 14:25:34 +0000

    function_pointers

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -5,11 +5,9 @@
 #include <stdlib.h>
 
 /**
- * struct op - Structure pour les opérateurs
- * @op: L'opérateur (caractère)
- * @f: La fonction associée à l'opérateur
- *
- * Description: Structure qui associe un opérateur à sa fonction
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
@@ -17,14 +15,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-/* Prototypes des fonctions d'opération */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-/* Prototype de la fonction qui sélectionne l'opération */
 int (*get_op_func(char *s))(int, int);
 
-#endif 
+#endif
