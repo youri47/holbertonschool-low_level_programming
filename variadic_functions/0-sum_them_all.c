d9cc3eb15191781641ad5a00dda11c879e5f52a6
youri47 <youri93400@gmail.com>
2025-11-19 14:54:20 +0000

    variadic_functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,9 +5,6 @@
  * sum_them_all - Calcule la somme de tous les parametres
  * @n: Nombre de parametres a additionner
  *
- * Description: Utilise des arguments variadiques pour additionner
- * un nombre variable de parametres entiers.
- *
  * Return: La somme de tous les parametres, ou 0 si n == 0
  */
 int sum_them_all(const unsigned int n, ...)
@@ -16,20 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	
 	if (n == 0)
 		return (0);
 
-	
 	va_start(args, n);
 
-	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
 
-	
 	va_end(args);
 
 	return (sum);
