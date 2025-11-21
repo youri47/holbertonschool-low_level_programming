3b3231a6fc2b7d9912a8b3b9d9187bd3d0405b47
youri47 <youri93400@gmail.com>
2025-11-21 16:08:05 +0000

    variadic_functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+#include <stdarg.h>
+
 /**
  * struct printer - Structure pour associer un type a une fonction
  * @symbol: Le caractere representant le type
@@ -15,6 +14,9 @@ typedef struct printer
 	void (*func)(va_list);
 } printer_t;
 
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
