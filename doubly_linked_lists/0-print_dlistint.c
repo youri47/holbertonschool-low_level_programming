ebd2461ea1961f4f292594d52f8f3965a4e72ff1
youri47 <youri93400@gmail.com>
2025-12-05 16:49:05 +0000

    doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,27 +1,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint - affiche tous les éléments d'une liste dlistint_t
- * @h: pointeur vers la tête de la liste
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
  *
- * Return: le nombre de noeuds
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t compteur = 0;
+	size_t count = 0;
 
-    /* Parcourir toute la liste */
-    while (h != NULL)
-    {
-        /* Afficher la valeur du noeud actuel */
-        printf("%d\n", h->n);
-        
-        /* Passer au noeud suivant */
-        h = h->next;
-        
-        /* Incrémenter le compteur */
-        compteur++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return (compteur);
+	return (count);
 }
