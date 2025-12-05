36d3348c590797dffbabafd37db478b86208b0ee
youri47 <youri93400@gmail.com>
2025-12-05 12:45:31 +0000

    singly linked

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,35 +1,33 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node - Ajoute un nouveau noeud au debut de la liste
- * @head: Double pointeur vers le premier noeud
- * @str: Chaine a stocker dans le nouveau noeud
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: Adresse du nouveau noeud, ou NULL si echec
+ * Return: the address of the new element, or NULL if it fails
  */
+
+
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+     list_t *new;
+ unsigned int len = 0;
 
-	if (str == NULL)
-		return (NULL);
+ while (str[len]){
+     len++;
+     }
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+ new = malloc(sizeof(list_t));
+ if (!new)
+ return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
+ new->str = strdup(str);                                /* struc de base y a str et unnsigned len , donc le */ 
+ new->len = len;                                       /*  1)str de new (new->str) dois etre = a qqchose ici  */
+ new->next = (*head);                                  /*  direct str ducoup stringduplicate . */
+ (*head) = new;                                  /* 2)y a len aussi du coup lelen de new doit = len et 3) le  node doit pointer a son next qui etait le  */ 
+ return (*head);                                       /*  1er node avant . */
 }
