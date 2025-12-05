58670c7d8daee65345f6f49424ee0d2e638ae69d
youri47 <youri93400@gmail.com>
2025-12-05 21:55:52 +0000

    singly list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,91 +1,31 @@
-#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
- * create_new_node - Crée un nouveau noeud pour la table de hachage
- * @key: La clé
- * @value: La valeur
+ * add_node - ajoute un nouveau nœud au début d'une liste chaînée
+ * @head: double pointeur vers la list_t liste
+ * @str: nouvelle chaîne à ajouter dans le nœud
  *
- * Return: Pointeur vers le nouveau noeud, ou NULL si échec
+ * Return: l'adresse du nouvel élément, ou NULL si échec
  */
-hash_node_t *create_new_node(const char *key, const char *value)
-{
-	hash_node_t *nouveau_noeud;
 
-	nouveau_noeud = malloc(sizeof(hash_node_t));
-	if (nouveau_noeud == NULL)
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	nouveau_noeud->key = strdup(key);
-	if (nouveau_noeud->key == NULL)
-	{
-		free(nouveau_noeud);
-		return (NULL);
-	}
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
-	nouveau_noeud->value = strdup(value);
-	if (nouveau_noeud->value == NULL)
-	{
-		free(nouveau_noeud->key);
-		free(nouveau_noeud);
-		return (NULL);
-	}
-
-	nouveau_noeud->next = NULL;
-	return (nouveau_noeud);
-}
-
-/**
- * update_value - Met à jour la valeur d'un noeud existant
- * @noeud: Le noeud à mettre à jour
- * @value: La nouvelle valeur
- *
- * Return: 1 en cas de succès, 0 sinon
- */
-int update_value(hash_node_t *noeud, const char *value)
-{
-	char *nouvelle_valeur;
-
-	nouvelle_valeur = strdup(value);
-	if (nouvelle_valeur == NULL)
-		return (0);
-
-	free(noeud->value);
-	noeud->value = nouvelle_valeur;
-	return (1);
-}
-
-/**
- * hash_table_set - Ajoute ou met à jour un élément
- * @ht: La table de hachage
- * @key: La clé (ne peut pas être vide)
- * @value: La valeur associée à la clé (peut être vide)
- *
- * Return: 1 en cas de succès, 0 sinon
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *nouveau_noeud, *temp;
-
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-			return (update_value(temp, value));
-		temp = temp->next;
-	}
-
-	nouveau_noeud = create_new_node(key, value);
-	if (nouveau_noeud == NULL)
-		return (0);
-
-	nouveau_noeud->next = ht->array[index];
-	ht->array[index] = nouveau_noeud;
-
-	return (1);
+	return (*head);
 }
