c04bf5372b1ba1d9ec56e32a6eeeb31cf6d41549
youri47 <youri93400@gmail.com>
2025-12-05 12:39:34 +0000

    singly linked

--- singly_linked_lists/main.c
+++ singly_linked_lists/main.c
@@ -1,70 +1,35 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * main - Point d'entree du programme de test
+ * main - Programme de test pour add_node
  *
  * Return: Toujours 0 (succes)
  */
 int main(void)
 {
-	list_t *head;
-	list_t *node1, *node2, *node3, *node4;
-	size_t n;
+	list_t *head = NULL;
 
-	/* Creation des noeuds manuellement pour le test */
-	node1 = malloc(sizeof(list_t));
-	node2 = malloc(sizeof(list_t));
-	node3 = malloc(sizeof(list_t));
-	node4 = malloc(sizeof(list_t));
+	printf("=== Ajout de noeuds au debut ===\n\n");
 
-	/* Verification des allocations */
-	if (!node1 || !node2 || !node3 || !node4)
-		return (1);
+	printf("Ajout de 'Third'...\n");
+	add_node(&head, "Third");
+	print_list(head);
+	printf("\n");
 
-	/* Remplissage du noeud 1 */
-	node1->str = strdup("Hello");
-	node1->len = 5;
-	node1->next = node2;
+	printf("Ajout de 'Second'...\n");
+	add_node(&head, "Second");
+	print_list(head);
+	printf("\n");
 
-	/* Remplissage du noeud 2 */
-	node2->str = strdup("World");
-	node2->len = 5;
-	node2->next = node3;
+	printf("Ajout de 'First'...\n");
+	add_node(&head, "First");
+	print_list(head);
+	printf("\n");
 
-	/* Remplissage du noeud 3 - avec str NULL pour tester ce cas */
-	node3->str = NULL;
-	node3->len = 0;
-	node3->next = node4;
-
-	/* Remplissage du noeud 4 */
-	node4->str = strdup("Holberton");
-	node4->len = 9;
-	node4->next = NULL;
-
-	/* La tete pointe vers le premier noeud */
-	head = node1;
-
-	/* Appel de la fonction a tester */
-	printf("=== Test de print_list ===\n");
-	n = print_list(head);
-	printf("-> Nombre de noeuds: %lu\n", n);
-
-	/* Test avec liste vide (NULL) */
-	printf("\n=== Test avec liste vide ===\n");
-	n = print_list(NULL);
-	printf("-> Nombre de noeuds: %lu\n", n);
-
-	/* Liberation de la memoire */
-	free(node1->str);
-	free(node2->str);
-	free(node4->str);
-	free(node1);
-	free(node2);
-	free(node3);
-	free(node4);
+	printf("=== Liste finale ===\n");
+	printf("Nombre de noeuds: %lu\n", list_len(head));
 
 	return (0);
 }
